9ac68a6fa34fe6c73a3b356a887cb1ff13c76355
Kevin Benevides <39235256801@info.local>
2019-04-09 11:35:27 -0300

    att

--- Front-End/nome-do-projeto/src/index.js
+++ Front-End/nome-do-projeto/src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Route, BrowserRouter as Router, Switch, Redirect } from "react-router-dom";
 import './index.css';
-import App from './App';
+import App from './pages/Home/App';
+import Desejos from './pages/Desejos/Desejos'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const routing = (
+    <Router>
+        <div>
+            <Switch>
+                <Route exact path ="/" component={App}/>
+                <Route path="/desejos" component={Desejos}/>
+            </Switch>
+        </div>
+    </Router>
+)
+
+ReactDOM.render(routing, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ Front-End/nome-do-projeto/src/pages/Desejos/Desejos.js
@@ -0,0 +1,76 @@
+import React, {Component} from 'react';
+
+import Axios from 'axios';
+
+class Desejos extends Component{
+    
+
+    constructor(){
+        var data = new Date();
+        var dia = data.getDate();
+        var mes = data.getMonth() + 1;
+        var ano = data.getFullYear();
+
+        var str_data = dia + '/' + mes + '/' + ano
+        super();
+        this.state = {
+            Lista : [],
+            desejos : "",
+            datacriacao : str_data
+        }
+
+        this.atualizaStateNome = this.atualizaStateNome.bind(this);
+    }
+
+    atualizaStateNome(event){
+        this.setState({desejos : event.target.value})
+    }
+
+    cadastraDesejo(event){
+        event.preventDefault();
+
+        Axios.post("http://localhost:5000/api/Desejo",{
+            descricao : this.state.desejos,
+            datacriacao : this.state.datacriacao
+
+        })
+
+        .then(data => {
+            if(data.status === 200){
+              console.log(data);
+            }else{
+                alert('Email ou senha inválido')
+              }
+            })
+            .catch(erro => {
+              console.log(erro);
+              this.setState({ erroMensegem : 'Email ou senha inválidos'})
+            })
+    }
+
+    render(){
+        return(
+                <div>
+                    <h1>Desejos</h1>
+                    <p>Bem vindo aos seus Desejos</p>
+                    <form onSubmit={this.cadastraDesejo}>
+                        <p>Qual o seu Desejo</p>
+                        <input 
+                        className="Caixa-desejos" 
+                        type="text" 
+                        value={this.state.desejos} 
+                        onChange={this.atualizaStateNome} 
+                        placeholder="Desejo..."/>
+                        
+                        <button>
+                            Cadastrar
+                        </button>
+
+                    </form>
+                </div>
+
+        )
+    }
+}
+
+export default Desejos;
